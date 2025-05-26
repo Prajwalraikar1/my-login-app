6d1b486327c802fa0f1376e87dc4c99d9aa71d02
Prajwalraikar1 <prajwalraikar1a@gmail.com>
2025-05-26 12:46:48 +0530

    Add basic React Vite login UI

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="login-box">
+      <h2>Login</h2>
+      <form>
+        <div className="input-box">
+          <input type="text" placeholder="Username" />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
